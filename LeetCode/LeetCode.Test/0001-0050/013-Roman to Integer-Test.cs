2fff36359a9b0ca7bcb491ea2c8da819db9352aa
Tanin <tanin.wanvirirat@gmail.com>
2024-01-13 17:33:13 +0700

    013

---
+++ LeetCode/LeetCode.Test/0001-0050/013-Roman to Integer-Test.cs
@@ -0,0 +1,81 @@
+﻿namespace LeetCode.Test
+{
+    public class _013_Roman_to_Integer_Test
+    {
+        [Test]
+        public void RomapToInteger_OneDigit()
+        {
+            var s = "III";
+            var solution = new _013_Roman_to_Integer();
+            var result = solution.RomanToInt(s);
+
+
+            Assert.That(result, Is.EqualTo(3));
+
+        }
+
+        [Test]
+        public void RomapToInteger_TwoDigit()
+        {
+            var s = "LVIII";
+            var solution = new _013_Roman_to_Integer();
+            var result = solution.RomanToInt(s);
+
+
+            Assert.That(result, Is.EqualTo(58));
+
+        }
+        
+
+        [Test]
+        public void RomapToInteger_ThreeDigit()
+        {
+            var s = "DLXVII";
+            var solution = new _013_Roman_to_Integer();
+            var result = solution.RomanToInt(s);
+
+            Assert.That(result, Is.EqualTo(567));
+
+        }
+
+        [Test]
+        public void RomapToInteger_FourDigit()
+        {
+            var s = "MCMXCIV";
+            var solution = new _013_Roman_to_Integer();
+            var result = solution.RomanToInt(s);
+
+
+            Assert.That(result, Is.EqualTo(1994));
+
+        }
+
+        [Test]
+        public void RomapToInteger_FailString()
+        {
+            var s = "Z";
+            var solution = new _013_Roman_to_Integer();
+
+            Assert.Catch<ArgumentException>(() => solution.RomanToInt(s),"String input contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').");
+        }
+
+        [Test]
+        public void RomapToInteger_StringEmpty()
+        {
+            var s = "";
+            var solution = new _013_Roman_to_Integer();
+
+            Assert.Throws<ArgumentException>(() => solution.RomanToInt(s), "String input length 1 to 15.");
+        }
+
+        [Test]
+        public void RomapToInteger_StringOverMaxLength15()
+        {
+            var s = "IIIIIIIIIIIIIIII";
+            var solution = new _013_Roman_to_Integer();
+
+            Assert.Throws<ArgumentException>(() => solution.RomanToInt(s), "String input length 1 to 15.");
+        }
+
+    }
+}
---
+++ LeetCode/LeetCode/0001-0050/013-Roman to Integer.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    public class _013_Roman_to_Integer
+    {
+        public int RomanToInt(string s)
+        {
+            var result = 0;
+            if (s.Length < 1 || s.Length > 15) throw new ArgumentException("String input length 1 to 15.");
+            var dic = new Dictionary<char, int>();
+            dic['I'] = 1;
+            dic['V'] = 5;
+            dic['X'] = 10;
+            dic['L'] = 50;
+            dic['C'] = 100;
+            dic['D'] = 500;
+            dic['M'] = 1000;
+
+            
+            for (int i = 0; i<s.Length; i++)
+            {
+                if (!dic.TryGetValue(s[i], out _)) throw new ArgumentException("String input contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').");
+                if (i != 0 && dic[s[i]] > dic[s[i - 1]])
+                {
+                    result = result + dic[s[i]] - (2 * dic[s[i - 1]]);
+                }
+                else
+                {
+                    result += dic[s[i]];
+                }
+            }
+
+            return result;
+        }
+    }
+}
