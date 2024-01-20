69c7f769f5440dd05bc8b913ac22a97c6a33bb18
Tanin <tanin.wanvirirat@gmail.com>
2024-01-20 22:12:24 +0700

    Palindrome

---
+++ LeetCode/LeetCode.Test/0001-0050/009-Palindrome Number-Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    internal class _009_Palindrome_Number_Test
+    {
+
+        [Test]
+        public void IsPalindrome_True()
+        {
+            var x = 121;
+
+            var solution = new _009_Palindrome_Number();
+            var result = solution.IsPalindrome(x);
+
+            Assert.That(result, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void IsPalindrome_LessThanZero()
+        {
+            var x = -121;
+
+            var solution = new _009_Palindrome_Number();
+            var result = solution.IsPalindrome(x);
+
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void IsPalindrome_False()
+        {
+            var x = 10;
+
+            var solution = new _009_Palindrome_Number();
+            var result = solution.IsPalindrome(x);
+
+            Assert.That(result, Is.EqualTo(false));
+        }
+    }
+}
---
+++ LeetCode/LeetCode/0001-0050/009-Palindrome Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0001_0050
+{
+    public class _009_Palindrome_Number
+    {
+        public bool IsPalindrome(int x)
+        {
+            int y  = 0,  copy = x;
+            if (x < 0 ) return false;
+
+            while (copy > 0)
+            {
+                y = (copy % 10) + (y * 10);
+                copy /= 10;
+            }
+            return x == y;
+        }
+    }
+}
