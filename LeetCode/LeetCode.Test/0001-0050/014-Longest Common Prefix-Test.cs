9a2aa33ca7f31e96b7835840b83809e801dd7095
Tanin <tanin.wanvirirat@gmail.com>
2024-01-22 21:50:51 +0700

    14 Longest Common Prefix

---
+++ LeetCode/LeetCode.Test/0001-0050/014-Longest Common Prefix-Test.cs
@@ -0,0 +1,57 @@
+﻿
+namespace LeetCode.Test
+{
+    public class _014_Longest_Common_Prefix_Test
+    {
+
+        [Test]
+        public void RomapToInteger_EmptyArray()
+        {
+            string[] s = [];
+            var solution = new _014_Longest_Common_Prefix();
+            var result = solution.LongestCommonPrefix(s);
+
+
+            Assert.That(result, Is.EqualTo(""));
+
+        }
+
+        [Test]
+        public void RomapToInteger_LengthStringNotEqual()
+        {
+            string[] s = ["ab", "a"];
+            var solution = new _014_Longest_Common_Prefix();
+            var result = solution.LongestCommonPrefix(s);
+
+
+            Assert.That(result, Is.EqualTo("a"));
+
+        }
+
+
+        [Test]
+        public void RomapToInteger_ZeroDigit()
+        {
+            string[] s = ["dog", "racecar", "car"];
+            var solution = new _014_Longest_Common_Prefix();
+            var result = solution.LongestCommonPrefix(s);
+
+
+            Assert.That(result, Is.EqualTo(""));
+
+        }
+
+        [Test]
+        public void LongestCommonPrefix_TwoDigit()
+        {
+            string[] s = ["flower", "flow", "flight"];
+            var solution = new _014_Longest_Common_Prefix();
+            var result = solution.LongestCommonPrefix(s);
+
+
+            Assert.That(result, Is.EqualTo("fl"));
+
+        }
+
+    }
+}
---
+++ LeetCode/LeetCode/0001-0050/014-Longest Common Prefix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public  class _014_Longest_Common_Prefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            string result = "";
+            int j = 0;
+            if (!strs.Any()) return result;
+            while (strs[0].Length  > j)
+            {
+                char _char = strs[0][j];
+                for (int i = 1; i < strs.Length ; i++)
+                {
+                    if (j >= strs[i].Length) return result;
+                    else if (strs[i][j] != _char)
+                    { 
+                        return result;
+                    }
+                }
+                j++;
+                result += _char;
+            }
+            return result;
+        }
+    }
+}
