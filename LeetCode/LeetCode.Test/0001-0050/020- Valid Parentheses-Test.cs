436ef1d2c7cc488bf08acbfa54645be3648647c6
Tanin <tanin.wanvirirat@gmail.com>
2024-01-27 21:52:06 +0700

    Valid Parentheses

---
+++ LeetCode/LeetCode.Test/0001-0050/020- Valid Parentheses-Test.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test
+{
+    public class _020_Valid_Parentheses_Test
+    {
+
+        [Test]
+        public void Valid_Parentheses_Empty_Parentheses()
+        {
+            string s = "";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.False);
+
+        }
+
+        [Test]
+        public void Valid_Parentheses_One_Parentheses_True()
+        {
+            string s = "()";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.True);
+
+        }
+
+        [Test]
+        public void Valid_Parentheses_Two_Parentheses_True()
+        {
+            string s = "({})";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.True);
+
+        }
+
+        [Test]
+        public void Valid_Parentheses_Two_Parentheses_And_AnyChar_True()
+        {
+            string s = "[Bo(o)M]";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.True);
+
+        }
+
+
+        [Test]
+        public void Valid_Parentheses_Three_Parentheses_True()
+        {
+            string s = "()[]{}";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.True);
+
+        }
+
+
+        [Test]
+        public void Valid_Parentheses_One_Parentheses_False()
+        {
+            string s = "(]";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.False);
+
+        }
+
+        [Test]
+        public void Valid_Parentheses_Two_Parentheses_False()
+        {
+            string s = "([)]";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.False);
+
+        }
+
+        [Test]
+        public void Valid_Parentheses_Three_Parentheses_False()
+        {
+            string s = "[B{o}(o]M)";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.False);
+
+        }
+
+        [Test]
+        public void Valid_Parentheses_One_Back_Parentheses_False()
+        {
+            string s = ")(";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.False);
+
+        }
+
+          [Test]
+        public void Valid_Parentheses_Three_Back_Parentheses_False()
+        {
+            string s = ")(){}";
+            var solution = new _020_Valid_Parentheses();
+            var result = solution.IsValid(s);
+
+            Assert.That(result, Is.False);
+
+        }
+    }
+
+}
---
+++ LeetCode/LeetCode/0001-0050/020-Valid Parentheses.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _020_Valid_Parentheses
+    {
+        public Dictionary<char, char> open = new Dictionary<char, char>
+        {
+            ['('] = ')',
+            ['{'] = '}',
+            ['['] = ']'
+        };
+
+        public Dictionary<char, char> close = new Dictionary<char, char>
+        {
+            
+            [')'] = '(',
+            ['}'] = '{',
+            [']'] = '['
+        };
+        public bool IsValid(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return false;
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                stack.TryPeek(out char tmpParenthese);
+                if (open.TryGetValue(c, out _))
+                {
+                    stack.Push(c);
+                }
+                else if (close.TryGetValue(c, out var value))
+                {
+                    if (value != tmpParenthese) return false;
+                    stack.Pop();
+                }
+            }
+            return !stack.Any();
+        }
+
+    }
+}
