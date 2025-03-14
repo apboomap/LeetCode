044cde98447b889e2f5ba89cff40d6d938dff410
Tanin <tanin.wanvirirat@gmail.com>
2025-03-14 23:18:17 +0700

    ข้อ 16

---
+++ LeetCode/LeetCode.Test/0001-0050/016-Three Sum Closest-Test.cs
@@ -0,0 +1,40 @@
+using LeetCode._0001_0050;
+
+namespace LeetCode.Test._0001_0050;
+
+public class _016_Three_Sum_Closeset_Test
+{
+    private _016_Three_Sum_Closeset _solution;
+    [SetUp]
+    public void SetUp()
+    {
+        _solution = new();
+    }
+
+    public static IEnumerable<TestCaseData> ThreeSumClosetTestCases()
+    {
+        yield return new TestCaseData(new int[] { -1, 2, 1, -4 }, 1, 2);
+        yield return new TestCaseData(new int[] { 0, 0, 0 }, 0, 0);
+        yield return new TestCaseData(new int[] { 0, 1, 2 }, 0, 3);
+        yield return new TestCaseData(new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90}, 1, 60);
+    }
+
+    [TestCaseSource(nameof(ThreeSumClosetTestCases))]
+    public void Three_Sum_AdditionalCases(int[] values, int target, int expected)
+    {
+        Assert.That(_solution.ThreeSumClosest(values, target), Is.EqualTo(expected));
+    }
+
+    // [TestCase]
+    // public void Three_Sum_Input_Empty()
+    // {
+    //     Assert.That(_solution.ThreeSum([]), Is.EquivalentTo(new List<List<int>>()));
+    // }
+
+    // [TestCase]
+    // public void Three_Sum_Input_Not_Three_Index()
+    // {
+    //     Assert.That(_solution.ThreeSum(new int[] { 0, 0}), Is.EquivalentTo(new List<List<int>>()));
+    // }
+    
+}
---
+++ LeetCode/LeetCode/0001-0050/016-Three Sum Closest.cs
@@ -0,0 +1,42 @@
+namespace LeetCode._0001_0050;
+
+public class _016_Three_Sum_Closeset
+{
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+        int result = int.MaxValue;
+        Array.Sort(nums);
+        int length = nums.Length;
+        int i = 0;
+        while (i <= length-3)
+        {
+            int left = i+1, right = length - 1;
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == target) return target;
+                if (Math.Abs(sum - target) < Math.Abs(result - target))  
+                {
+                    result = sum;
+                }
+
+                if (sum < target) 
+                {
+                    left++;
+                }
+                else
+                {
+                    right --;
+                }
+            }
+           
+            do
+            {
+                i++;
+            }
+            while(i<=length-3 && nums[i] == nums[i - 1]);
+        }
+
+        return result;
+    }
+}
